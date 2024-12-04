6f462e383ffedb83487efebd6316ccf480f6475b
Jonathan Bartolo <jonathan.bartolo@stmartin.edu>
2024-12-03 16:02:35 -0800

    Added database

---
+++ StudentManagementSystem/Models/Student.cs
@@ -0,0 +1,8 @@
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string RollNo { get; set; }
+    public string Section { get; set; }
+    public string Program { get; set; }
+}
